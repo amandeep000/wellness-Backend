79513f78108ce1c8672c34a51f59c4cf3ce3c6ce
amandeep <deepaman0556@gmail.com>
2025-08-20 11:53:16 +0530

    fix:add start script in package json

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const mongodb_database = "ecommerce";
--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
+import { mongodb_database } from "../constants.js";
 
 const dbConnect = async () => {
   try {
-    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+    const connectionInstance = await mongoose.connect(
+      process.env.MONGODB_URI + mongodb_database
+    );
     console.log(
       "\n connected to db,connection host: ",
       connectionInstance.connection.host
     );
   } catch (error) {
-    console.error("failed to connect mongodbAtlas", {
+    console.error("failed to connect mongodb Atlas", {
       message: error.message,
       stack: error.stack,
     });
