46dda04f2b6c858d91f0fc9b2aac0f2791597e6b
amandeep <deepaman0556@gmail.com>
2025-08-23 20:14:58 +0530

    Update: change http method of logout endpoint from get to post

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -10,7 +10,7 @@ const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/logout").get(verifyJWT, logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(createNewAccessAndRefreshToken);
 
 export { router };
